1034223f771f96c84d20ff20abbed1dcd47cd9e3
Adam Merberg <amerberg@gmail.com>
2018-07-03 22:55:44 -0400

    Move settings inline

--- app/run.py
+++ app/run.py
@@ -1,16 +1,25 @@
 from eve import Eve
 from eve_sqlalchemy import SQL
 from eve_sqlalchemy.validation import ValidatorSQL
-from schema import Base
+from eve_sqlalchemy.config import DomainConfig, ResourceConfig
+from schema import Base, User
 import json
+import os
 
 
 def parse_field(resource, response):
     """Parse the JSON field, and convert keys to strings in case the user wants XML output."""
     response['most_similar'] = {"most_similar_{}".format(k): v for k, v in json.loads(response['most_similar']).items()}
 
+settings = {
+    'SQLALCHEMY_DATABASE_URI': os.environ['USER_SIMILARITY_DB_URL'],
+    'SQLALCHEMY_TRACK_MODIFICATIONS': False,
+    'DOMAIN': DomainConfig({
+        'user': ResourceConfig(User)
+    }).render()
+}
 
-app = Eve(validator=ValidatorSQL, data=SQL)
+app = Eve(validator=ValidatorSQL, data=SQL, settings=settings)
 Base.metadata.bind = app.data.driver.engine
 app.data.driver.Model = Base
 app.on_fetched_item += parse_field
